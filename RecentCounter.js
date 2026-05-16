09cad2bd429f6e0f2d0686096ab88a0da80ceaa1
PedroHenrique-Git <pedromf34@outlook.com>
2026-05-15 22:30:55 -0300

    RecentCounter

---
+++ RecentCounter.js
@@ -0,0 +1,22 @@
+var RecentCounter = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  this.queue.push(t);
+
+  const start = t - 3000;
+  const end = t;
+
+  return this.queue.filter((e) => e >= start && e <= end).length;
+};
+
+const recentCounter = new RecentCounter();
+console.log(recentCounter.ping(1));
+console.log(recentCounter.ping(100));
+console.log(recentCounter.ping(3001));
+console.log(recentCounter.ping(3002));
